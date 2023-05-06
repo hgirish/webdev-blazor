495b54749f29cce36ddd8441f82441bb104a1c17
hgirish <girish@post.com>
2023-05-05 22:12:20 -0700

    Edit categories

---
+++ MyBlog/Components/RazorComponents/CustomCssClassProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Components.RazorComponents;
+
+public class CustomCssClassProvider<ProviderType> : ComponentBase
+    where ProviderType : FieldCssClassProvider, new()
+{
+    [CascadingParameter]
+    EditContext? CurrentEditContext { get; set; }
+    public ProviderType Provider { get; set; } = new ProviderType();
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext == null)
+        {
+            throw new InvalidOperationException(
+                $"{nameof(CustomCssClassProvider<ProviderType>)} requires a cascading parameter of type {nameof(EditContext)}, for example, you can use {nameof(CustomCssClassProvider<ProviderType>)} inside and EditForm");
+
+        }
+        CurrentEditContext.SetFieldCssClassProvider(Provider);
+    }
+}
