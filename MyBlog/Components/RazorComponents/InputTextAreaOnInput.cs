369d132cd27d0dccfcfa81639dde0086361f566a
hgirish <girish@post.com>
2023-05-06 21:36:01 -0700

    Edit blogpost

---
+++ MyBlog/Components/RazorComponents/InputTextAreaOnInput.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.RazorComponents;
+public class InputTextAreaOnInput : InputBase<string>
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "textarea");
+        builder.AddMultipleAttributes(1, AdditionalAttributes);
+        builder.AddAttribute(2, "class", CssClass);
+        builder.AddAttribute(3, "value", BindConverter.FormatValue(CurrentValue));
+        builder.AddAttribute(4, "oninput",
+            EventCallback.Factory.CreateBinder<string>(
+                this, __value => 
+                CurrentValueAsString = __value, CurrentValueAsString));
+        builder.CloseElement();
+    }
+    protected override bool TryParseValueFromString(
+        string? value,
+        [MaybeNullWhen(false)] out string result,
+        [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        result = value;
+        validationErrorMessage = null;
+        return true;
+    }
+}
