cbee336d36d40b0a95879f15b55198cd1fd7efc9
Girish <girish@post.com>
2023-10-01 22:19:43 -0700

    Auth0 Authentication

--- MyBlog/BlazorWebAssembly/Server/Program.cs
+++ MyBlog/BlazorWebAssembly/Server/Program.cs
@@ -1,6 +1,10 @@
+using Auth0.AspNetCore.Authentication;
 using BlazorWebAssembly.Server.Endpoints;
+using Components.RazorComponents;
 using Data;
 using Data.Models.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +22,14 @@ builder.Services.AddOptions<BlogApiJsonDirectAccessSetting>()
         options.CategoriesFolder = "Categories";
     });
 builder.Services.AddScoped<IBlogApi, BlogApiJsonDirectAccess>();
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Authority"] ?? "";
+    options.ClientId = builder.Configuration["Auth0:ClientId"] ?? "";
+});
+
+builder.Services.AddTransient<ILoginStatus, LoginStatus>();
 
 var app = builder.Build();
 
@@ -40,6 +52,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapBlogPostApi();
 app.MapCategoryApi();
 app.MapTagApi();
@@ -47,5 +62,29 @@ app.MapTagApi();
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
+
+app.MapGet("authentication/login", async (string redirectUri, 
+    HttpContext context) =>
+{
+    var authenticationProperties = 
+    new LoginAuthenticationPropertiesBuilder()
+    .WithRedirectUri(redirectUri)
+    .Build();
+    await context.ChallengeAsync(Auth0Constants.AuthenticationScheme, 
+        authenticationProperties);
+});
+
+app.MapGet("authentication/logout", async (HttpContext context) =>
+{
+    var authenticationProperties = 
+    new LogoutAuthenticationPropertiesBuilder()
+    .WithRedirectUri("/")
+    .Build();
+    await context.SignOutAsync(Auth0Constants.AuthenticationScheme,
+        authenticationProperties);
+    await context.SignOutAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
 
 app.Run();
